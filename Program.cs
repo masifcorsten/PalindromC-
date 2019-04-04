1765b596088ddcbc9737f5852bd0aaaff79c7aed
masifcorsten <49274074+masifcorsten@users.noreply.github.com>
2019-04-04 12:49:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string str = "", ruekw = "", wort=""; 
+            int w = 0;
+            Console.WriteLine("Geben Sie ein Wort ein");
+            str = Console.ReadLine();
+            wort = str;
+            w = str.Length - 1;
+            while (w>= 0)
+            {
+                ruekw = ruekw + wort[w];
+                w--;
+            }
+            Console.WriteLine("Eingegebenes Wort ist = " + wort);
+            Console.WriteLine("Umgekehrt ist es = " + ruekw);
+            if (ruekw == wort)
+            {
+                Console.WriteLine("Das Wort ist ein Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Das Wort ist kein Palindrom");
+            }
+            Console.ReadLine();
+        }
+    
+    }
+}
